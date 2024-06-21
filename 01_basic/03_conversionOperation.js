d8595065a7ede95134cf6861257edf24ba2ee995
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-21 12:08:10 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//let score = 33 // it is number
+let score = "33" // in double quotes it is string 
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+
+
+//data conversion
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 ; false => 0
+
+
+//boolean
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "vikas" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+

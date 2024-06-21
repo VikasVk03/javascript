381f1aadd6c8bf48427c60e89fd99c1b3f98c56f
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-21 15:26:29 +0000

    operation done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -27,6 +27,42 @@ let booleanIsLoggedIn = Boolean(IsLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
 
+//***************** Operations *******************/
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // 2 topower 2
+// console.log(2%2)
+// console.log(2/2)
+
+let str1 = "hello"
+let str2 = " vikas"
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2" + 3 + 2);
+// console.log( 3 + 3 * 5 % 6); // it is not good to write and not accepted in industries, code should be easy to read and not to create any confusions // it is only asked in exam.
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, nem2, num3
+num1 = num2 = num3 = 2 + 2; // this is not good practice, if value is 4, donot make it 2 + 2
+
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+
+console.log(gameCounter);

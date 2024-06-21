a10a4b7e15730e54fc116dbf6f56537b45f62d8f
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-21 16:02:13 +0000

    comparison done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2" > 1);
+//console.log("2" < 1);
+
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 1);
+
+ 
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 1);
+
+/// === it is strict check, it checks both datatype and values
+console.log("2" === 2); // false as "2" is string and 2 is number
+
+

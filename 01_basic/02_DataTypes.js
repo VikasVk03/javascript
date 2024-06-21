cb6a68c5bb94ab7d4f79d87f94b16604507607c8
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-21 11:24:20 +0000

    datatypes

--- 01_basic/02_DataTypes.js
+++ 01_basic/02_DataTypes.js
@@ -1,1 +1,30 @@
 "use strict"  // treat all JS code as new version
+
+//alert("hello");  // we are using node js, not browser
+
+console.log(3
+    +3) //code readibility should be high
+
+console.log("vikas")
+
+let name ="vikas"
+let age =18
+let IsLoggedIn = false
+let state;
+
+
+//datatypes
+// number => 2 to power 53
+// bigInt // use for large website like trading site where number is big
+// string => "" // write its value in double quotes 
+// boolean => true/false
+// null => standalone value // it is representation of empty value // its datatype is object
+// undefined => 
+// symbols => uniqueness
+
+
+// object 
+
+console.log(typeof IsLoggedIn) // console.log(typeof ___)// use to know datatype
+console.log(typeof null) // datatype is object
+console.log(typeof undefined) // datatype is undefined

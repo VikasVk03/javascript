535f4e52214f45df8c51ace13855fcaaf1c7df99
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-22 06:59:10 +0000

    memory type explain

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -25,12 +25,41 @@ const BigNumber = 9839865679891235544423463243n
 // Array, Object, Function
 
 const heros = ["shaktimaan","nagaraj","doga"] // array are written in block braket
-let myObj = { // object is written in curly braket
-    name: vikas,
-    age: 21,
-}
+
+
+// let myObj = { // object is written in curly braket
+//     name: vikas,
+//     age: 21,
+// }
 
 //function(){} // function syntax
 const myFunction = function(){
     console.log("hello world");
 }
+
+
+// ++++++++++++++ Memory type+++++++++++++++++++\\
+
+
+// Stack(Primitive), when this memory is used, you will get copy of declared varible, original value is not change.
+// Heap(non-Primitive), you will get reference of declared value, whatever changes is made, original value will also change.
+
+let myYouTubeName = "hiteshchoudhrydotcom"
+
+let anotherName = myYouTubeName
+anotherName = "chaiorcode"
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@googel.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "histesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);

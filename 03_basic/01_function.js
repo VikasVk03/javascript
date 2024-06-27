642fb0ce37ecca7dfade2447f9c33548865d4e24
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-06-27 08:00:37 +0000

    1stfunction

---
+++ 03_basic/01_function.js
@@ -0,0 +1,12 @@
+// function
+function sayMyFunction(){
+    console.log("v");
+    console.log("i");
+    console.log("k");
+    console.log("a");
+    console.log("s");
+}
+
+// sayMyFunction - its reference,    sayMyFunction() - excute when typed () after name of function
+
+sayMyFunction()

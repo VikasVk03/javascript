48abdc4bae10290c253b615fb83f13b8e2967167
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-07-01 07:21:07 +0000

    scopedone

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -8,3 +8,44 @@ if (true) { // code written in {} has block scope
     var c = 30 
 }
 
+
+
+
+function one(){
+   const username = "vikas"
+
+   function two(){
+    const website = "youtube"
+    console.log(username);
+   }
+
+   // console.log(website);
+
+   // two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "vikas"
+    if (username === "vikas") {
+        const website = " youtube"
+        // console.log(username + website); 
+    }
+    // console.log(website)    
+}
+
+// console.log(username) 
+
+function addOne(num){
+    return num + 1
+}
+
+addOne(5)
+
+const addTwo =  function (num) {
+    return num + 2
+}
+
+console.log(addTwo(5))

79e7c7d5f991ca7b179de4676db061bf4c22eaab
VikasVk03 <vishwakarmavikas0102@gmail.com>
2024-07-07 17:35:59 +0000

    js_definition

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,3 +1,5 @@
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
 const accountId = 144553
 let accountEmail = "vikas@goole.come"
 var accountPassword = "12345"

f4d54ada082a0d5ffda52c9be73c5d42b6648ee2
VIKAS VAKIL VISHWAKARMA <vishwakarmavikas0102@gmail.com>
2024-08-10 18:06:14 +0000

    while_loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while loop
+// while (condition) {
+    
+// }
+
+// let index = 0
+// while (index <= 10) {
+//    index = index + 2; // preincrement
+//    console.log(`value of index is ${index}`);
+//    index = index +2; // postincrement
+// }
+
+// let myArray = ["flash", "batman", "superhero"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value of array is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+// do while loop
+
+let score = 1
+do {
+    console.log(`value of score is ${score}`)
+    score++
+} while (score < 10);

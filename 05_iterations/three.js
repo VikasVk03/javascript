0dfe4c3a570173cfb21d23ad53992d7dbc540517
VIKAS VAKIL VISHWAKARMA <vishwakarmavikas0102@gmail.com>
2024-08-15 10:09:58 +0000

    map_iteration

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -12,8 +12,33 @@ for (const num of arr) { // ye loop apne app pata laga lega kab usko end hona hi
 
 const greetings = "Hello world!"
 for (const greet of greetings) { // prints single words till end
-    console.log(`Each character is ${greet}`);
+    //console.log(`Each character is ${greet}`);
 }
 for (const greet of greetings) { // prints whole string jitna word count hi yaha 12 characters hi including space toh 12 time print krta hi.
-    console.log(`Each character is ${greetings}`);
+   // console.log(`Each character is ${greetings}`);
+}
+
+// Map it can have only unique value and follows sequence
+const map = new Map()
+map.set("IN", "INDIA")
+map.set("USA", "UNITED STATES OF AMERICA")
+map.set("FR", "FRANCE")
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
+for (const [key, value] of map) { // [key, value] ye array ka destructure hojata hi.
+    // console.log(key, "->", value);
+}
+
+const myObject = {
+    "game1": "Cod",
+    "game2": "GTA5",
+    "game3": "RE7"
+}
+// output:- TypeError: myObject is not iterable means in this way we can not iterate object.
+for (const [key, value] of myObject) {
+    console.log(key, ":-", value)
 }
